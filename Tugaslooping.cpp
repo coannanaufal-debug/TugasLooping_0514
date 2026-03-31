7bae0620c3569c2f88193cfe7179e70dc59082ff
coannanaufal-debug <coanna.naufal@gmail.com>
2026-03-31 11:07:11 +0700

    Function Pengecekan Bilangan Prima

--- Tugaslooping.cpp
+++ Tugaslooping.cpp
@@ -6,3 +6,7 @@ using namespace std;
 // === Deklarasi Variabel Global ===
 int angkaInput;
 int pilihanMenu;
+// === Function Pengecekan Bilangan Prima (bool) ===
+bool apakahPrima(int n) {
+    if (n <= 1) return false;
+    int i = 2;
